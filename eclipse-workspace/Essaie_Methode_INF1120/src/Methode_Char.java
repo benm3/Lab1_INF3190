e83f082d1744b48590c7005bbde46ded3c27ef5a
Mehdi Bennani <ck891031@ens.uqam.ca>
2020-01-28 11:42:41 -0500

    Ajout du code Methode

---
+++ eclipse-workspace/Essaie_Methode_INF1120/src/Methode_Char.java
@@ -0,0 +1,28 @@
+
+public class Methode_Char {
+
+	public static char validerChoixMenu (String msgSoll, String msgErr,
+			 char borneInf, char borneSup) {
+			 char choixMenu;
+			 System.out.print ("\n" + msgSoll);
+			 choixMenu = Clavier.lireCharLn();
+
+			 while (choixMenu < borneInf || choixMenu > borneSup) {
+			 System.out.println(msgErr);
+			 System.out.print (msgSoll);
+			 choixMenu = Clavier.lireCharLn();
+			 }
+
+			 return choixMenu;
+			}
+	
+	
+	public static void main(String[] args) {
+		
+		char lettre = validerChoixMenu("Veuillez saisir une lettre entre a et v : ",
+				"Erreur! Veuillez saisir une lettre valide", 'a', 'v');
+		
+		System.out.println("\nLe choix du menu est : " +lettre);
+	}
+
+}
